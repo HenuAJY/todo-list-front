67c4d2c088198be0df2522ce77c5b679df1ef2cc
henuajy <henuajy@163.com>
2024-05-05 18:09:35 +0800

    支持electron

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("./dist/index.html");
+};
+
+(async () => {
+  await app.whenReady();
+  createWindow();
+})();
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
